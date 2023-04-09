f96e43627637e432af58cbce1740ae3804a34e43
Guilherme de Liz <gliz@aawz.com.br>
2023-04-09 18:55:38 -0300

    feat: visual identity and forecast

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ function setWeather() {
       (position) => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-
+        setForecast({latitude, longitude});
         fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=pt_br&units=metric`
         )
@@ -43,8 +43,8 @@ function setWeather() {
               const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
 
               weather.innerHTML = `
-              <div class="info-weather">
-              <img src="${weatherIconUrl}" alt="${descriptionCapitalized}"/> ${descriptionCapitalized} e temperatura de ${tempFormatted}°C em ${city}
+              <div class="info-weather full-center" style="flex-direction: row;">
+              <img src="${weatherIconUrl}" alt="${descriptionCapitalized}"  title="${descriptionCapitalized}"/> ${tempFormatted}°C | ${city}
               </div>`;
             } else {
               weather.textContent =
@@ -68,6 +68,58 @@ function setWeather() {
   } else {
     weather.textContent = "Geolocalização não suportada pelo navegador";
   }
+}
+
+const setForecast = ({latitude, longitude}) => {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=pt_br&units=metric`)
+  .then((r) => r.json())
+  .then(r => {
+    console.log(r);
+
+    const el = document.createElement('div');
+    el.classList.add('card');
+    el.style.flexDirection = 'row';
+    el.style.gap = '15px';
+
+    const dates = [];
+
+    r.list.forEach(item => {
+      const dt = item.dt_txt.split(' ')[0];
+      if (!dates.includes(dt)) {
+        dates.push(dt);
+        // Get day/month from date
+        let dtTxt = new Date(item.dt_txt).toLocaleDateString('pt-BR').split('/');
+        dtTxt = `${dtTxt[0]}/${dtTxt[1]}`;
+
+        const description = item.weather[0].description;
+        const descriptionCapitalized =
+          description.charAt(0).toUpperCase() + description.slice(1);
+        const temp = item.main.temp;
+        const tempFormatted = temp.toFixed(0);
+        const weatherIconCode = item.weather[0].icon;
+        const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
+  
+        const html = `
+        <div class="full-center">
+        <img src="${weatherIconUrl}" alt="${descriptionCapitalized}" title="${descriptionCapitalized}"/>
+        <small>${tempFormatted}ºC</small>
+        <small style="color: var(--text-secondary)">${dtTxt}</small>
+        </div>
+        `;
+  
+        el.innerHTML += html;
+      }
+    });
+
+
+    console.log(el);
+    const forecast = document.getElementById('forecast');
+    forecast.appendChild(el);
+    forecast.style.display = 'flex';
+  })
+  .catch((error) => {
+    console.error(error);
+  })
 }
 
 window.onload = function () {
